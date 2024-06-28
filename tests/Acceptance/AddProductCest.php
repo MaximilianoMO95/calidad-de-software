874d37918786cd483220a3d78828b83d4c36a63c
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-28 17:20:58 -0400

    test: added more cases for try to add product

--- tests/Acceptance/AddProductCest.php
+++ tests/Acceptance/AddProductCest.php
@@ -59,4 +59,76 @@ class AddProductCest
         $I->click('#guardar_datos');
         $I->see('Error!');
     }
+
+    public function tryToTestAddProductWithoutCategory(AcceptanceTester $I): void
+    {
+        $I->amOnPage(CommonCest::STOCK_PAGE);
+        $I->click('#nuevoProducto');
+
+        $I->fillField('codigo', 'M005');
+        $I->fillField('nombre', 'Clavo');
+        $I->selectOption('categoria', '');
+        $I->fillField('precio', '2000');
+        $I->fillField('stock', '10');
+
+        $I->canSee('#guardar_datos');
+        $I->click('#guardar_datos');
+        $I->see('Error!');
+    }
+
+    public function tryToTestAddProductWithNotNumericPrice(AcceptanceTester $I): void
+    {
+        $I->amOnPage(CommonCest::STOCK_PAGE);
+        $I->click('#nuevoProducto');
+
+        $I->fillField('codigo', 'M005');
+        $I->fillField('nombre', 'Clavo');
+        $I->selectOption('categoria', '12');
+        $I->fillField('precio', 'no a number');
+        $I->fillField('stock', '10');
+
+        $I->canSee('#guardar_datos');
+        $I->click('#guardar_datos');
+        $I->see('Error!');
+    }
+
+    public function tryToTestAddProductWithSameName(AcceptanceTester $I): void
+    {
+        $I->haveInDatabase(
+            'products', [
+            'codigo_producto' => 'M002',
+            'nombre_producto' =>  'Clavo',
+            'date_added' => '2021-09-20 15:53:56',
+            'precio_producto' => 5990,
+            'stock' => 10,
+            'id_categoria' => 12,
+            ]
+        );
+
+        $I->amOnPage(CommonCest::STOCK_PAGE);
+        $I->click('#nuevoProducto');
+
+        $I->fillField('codigo', 'M002');
+        $I->fillField('nombre', 'Clavo');
+        $I->selectOption('categoria', '12');
+        $I->fillField('precio', '2000');
+        $I->fillField('stock', '1');
+
+        $I->canSee('#guardar_datos');
+        $I->click('#guardar_datos');
+        $I->see('Error!');
+
+        $I->amOnPage(CommonCest::STOCK_PAGE);
+        $I->click('#nuevoProducto');
+
+        $I->fillField('codigo', 'M002');
+        $I->fillField('nombre', 'clavo');
+        $I->selectOption('categoria', '12');
+        $I->fillField('precio', '2000');
+        $I->fillField('stock', '1');
+
+        $I->canSee('#guardar_datos');
+        $I->click('#guardar_datos');
+        $I->see('Error!');
+    }
 }
