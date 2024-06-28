6cf676dd2860494adc0c7664a3eb541fd84bc412
mr-ema <ema.a.v.c2017@gmail.com>
2024-06-28 12:39:24 -0400

    test: fixed uni tests and added integration tests

--- tests/Integration/LoginTest.php
+++ tests/Integration/LoginTest.php
@@ -1,18 +1,35 @@
 <?php
 
 
-namespace Tests\Unit;
+namespace Tests\Integration;
 
-use Tests\Support\UnitTester;
+use Tests\Support\IntegrationTester;
 
 require_once __DIR__ . '/../../classes/Login.php'; use Login;
 
+// Temporal solution until refactor the messy codebase
+define('DB_USER', 'root');
+define('DB_HOST', '127.0.0.1');
+define('DB_PASS', '');
+define('DB_NAME', 'testdb');
+
 class LoginTest extends \Codeception\Test\Unit
 {
-    protected UnitTester $tester;
+    protected IntegrationTester $tester;
+    protected int $user_id;
 
     protected function _before(): void
     {
+        $this->user_id = $this->tester->haveInDatabase(
+            'users', [
+            'firstname' => 'John',
+            'lastname' => 'Wick',
+            'user_name' => 'tank',
+            'user_email' => 'john@wick.com',
+            'user_password_hash' => '$2y$10$MPVHzZ2ZPOWmtUUGCq3RXu31OTB.jo7M9LZ7PmPQYmgETSNn19ejO',
+            'date_added' => '2024-03-12 15:06:00',
+            ]
+        );
     }
 
     protected function _after(): void
@@ -48,6 +65,20 @@ class LoginTest extends \Codeception\Test\Unit
 
         $login = new Login();
         $this->assertNotEmpty($login->errors, 'Expected an error got nothing');
+    }
+
+    public function testLoginWithValidDataDb(): void
+    {
+        $_POST['login'] = true;
+        $_POST['user_name'] = 'tank';
+        $_POST['user_password'] = 'admin';
+
+        $login = new Login();
+
+        $this->assertEmpty($login->errors, 'Expected not errors in correct login');
+        $this->assertTrue(session_status() !== PHP_SESSION_NONE);
+        $this->assertTrue($_SESSION['user_id'] == $this->user_id);
+        $this->assertTrue($_SESSION['user_login_status'] == 1);
     }
 
     public function testLogout(): void
